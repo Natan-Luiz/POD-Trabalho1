5a3a454ebd090064af05f8159b0f901b6f76551a
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2018-04-30 00:55:50 -0300

    inicio da aplicação

---
+++ application.cpp
@@ -0,0 +1,21 @@
+/*
+//   TRABALHO 1 DE POD
+//
+// Denes Vargas e Natan Berwaldt
+//
+//
+*/
+
+#define MAX_ARQS 10                           // Maximo de arquivos a serem utilizados
+#define ENTRADA_INICIAL "entrada.txt"         // Link para o arquivo de entrada
+
+int main(void)
+{
+  mergeSortExternal(ENTRADA_INICIAL);
+  return;
+}
+
+void mergeSortExternal(string link)
+{
+  
+}
