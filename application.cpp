cdc14cbf917a675182b8ee2f6856421b1c557649
Natan-Luiz <37088299+Natan-Luiz@users.noreply.github.com>
2018-05-10 14:20:41 -0300

    Atualziado com inicio da implementacao
    Falta implementar MergeSort e Intercalação de N caminhos

--- application.cpp
+++ application.cpp
@@ -6,16 +6,93 @@
 //
 */
 
-#define MAX_ARQS 10                           // Maximo de arquivos a serem utilizados
-#define ENTRADA_INICIAL "entrada.txt"         // Link para o arquivo de entrada
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include<string>
+#include <sstream>
 
-int main(void)
+
+#define NumeroArquivos 4
+#define MemoriaRAM     3 //1 MB de memoria
+
+#define ENTRADA "entrada.txt"
+
+using namespace std;
+
+void grava(int vetor[],int nome_arq);
+void Juntar(int vetor[], int ini, int meio, int fim, int vetAux[]);
+void MergeSort(int vetor[], int inicio, int fim, int vetorAux[]);
+void MergeSort(int vetor[], int tamanho);
+
+int main()
 {
-  mergeSortExternal(ENTRADA_INICIAL);
-  return;
+   cout<<"Carregando...."<<endl;
+
+   fstream fp;
+   fp.open(ENTRADA);
+
+
+   int v[MemoriaRAM];
+   int i = 0, j =0;;
+   while(fp >> v[i])
+   {
+        i++;
+        if(i==3)
+        {
+            MergeSort(v,MemoriaRAM);
+            i=0;
+            if(j==NumeroArquivos/2) j=0;
+            grava(v,j);
+            j++;
+        }
+   }
+   //interpolacao();
+
+   return 0;
 }
 
-void mergeSortExternal(string link)
+void grava(int vetor[],int nome_arq)
 {
-  
+    fstream fout;
+    stringstream num;
+    string endereco;
+    int i = 0;
+    endereco = "in";
+    num << nome_arq;
+    endereco = endereco+num.str();
+    fout.open(endereco, fstream::in | fstream::out | fstream::app);
+    while(i < MemoriaRAM)
+    {
+        fout << vetor[i]<<" ";
+        i++;
+    }
+    fout.close();
+}
+
+void interpolacao()
+{
+    fstream in[NumeroArquivos/2];
+    fstream out[NumeroArquivos/2];
+
+
+
+    while(int i=0 < NumeroArquivos/2)
+    {
+        stringstream num = i;
+        string enderecoIn= "in"+num;
+        in[i].open(enderecoIn, fstream::in);
+    }
+
+    while(int i=0 < NumeroArquivos/2)
+    {
+        stringstream num = i;
+        string enderecoOut= "in"+num;
+        out[i].open(enderecoOut, fstream::out);
+    }
+
+    while(int i=0 < NumeroArquivos/2)
+    {
+        s
+    }
 }
