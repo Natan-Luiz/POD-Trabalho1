e9bacdf104b65994ff593fca6e7c61d94e449943
Denesvargas <37148037+Denesvargas@users.noreply.github.com>
2018-05-11 02:03:54 -0300

    Update application.cpp

--- application.cpp
+++ application.cpp
@@ -6,18 +6,17 @@
 //
 */
 
-// Esta primeira versão está sendo feita ordenando Números, 
+// Esta primeira versão está sendo feita ordenando Números,
 // para entender bem o conceito da ordenação externa, pra depois trocar por palavras.
 
 #include<iostream>
 #include<fstream>
 #include<vector>
 #include<string>
-#include <sstream>
-
+#include<sstream>
 
 #define NumeroArquivos 4
-#define MemoriaRAM     3 //1 MB de memoria
+#define MemoriaRAM     3 // vai mudar para 1 MB de memoria
 
 #define ENTRADA "entrada.txt"
 
@@ -25,60 +24,67 @@ using namespace std;
 
 void grava(int vetor[],int nome_arq);
 void Juntar(int vetor[], int ini, int meio, int fim, int vetAux[]);
-void MergeSort(int vetor[], int inicio, int fim, int vetorAux[]);
-void MergeSort(int vetor[], int tamanho);
+int quicksort(int *a, int inicio, int fim);
+int particiona(int *a, int inicio, int fim);
 
 int main()
 {
    cout<<"Carregando...."<<endl;
-
    fstream fp;
    fp.open(ENTRADA);
-
+   if(fp.is_open() == false)
+        std::cout << "Nao foi possivel abrir arquivo entrada" << std::endl;
 
    int v[MemoriaRAM];
-   int i = 0, j =0;;
+   int i = 0, j = 0;
    //enquanto houver dados faça:
-   while(fp >> v[i])
+   while(!fp.eof())
    {
+        fp >> v[i];
+        cout << "ta no while" << v[i] << endl;
         i++;
-        if(i==MemoriaRAM)                       //quando i = memoria ram, faz o merge e poe no arquivo
+        if(i==MemoriaRAM)                       //quando i = memoria ram, faz o quick e poe nos buffers temporários
         {
-            MergeSort(v,MemoriaRAM);
+            quicksort(v, 0, MemoriaRAM-1);
             i=0;
             if(j==NumeroArquivos/2) j=0;
             grava(v,j);
             j++;
         }
    }
+   //erro do que sobra do vetor --> corrigir
+   for(int k = 0; k < 3; k++)
+     cout << v[k] << endl;
    //interpolacao();
 
    return 0;
 }
 
-void grava(int vetor[],int nome_arq)
-{
-    fstream fout;       //string contendo o endereco do arquivo
-    stringstream num;   //string contendo o numero do arquivo
-    string endereco;    //string contendo o nome do arquivo
+void grava(int vetor[],int nome_arq){
+    fstream fw;           //variavel FILE contendo o endereco do buffer
+    stringstream num;     //string contendo o numero do buffer temporário
+    string endereco;      //string contendo o nome do buffer temporário
     int i = 0;
-    endereco = "in";
+    endereco = "temp";
     num << nome_arq;
     endereco = endereco+num.str();
-    fout.open(endereco, fstream::in | fstream::out | fstream::app);
-   //passa os numeros do vetor para o arquivo
+    endereco += ".txt";
+    //passa os numeros do vetor para o arquivo
+    const char *nome = endereco.c_str();
+    //cria o arquivo temp
+    fw.open(nome, fstream::in | fstream::out | fstream::app);
     while(i < MemoriaRAM)
     {
-        fout << vetor[i]<<" ";
+        fw << vetor[i]<<" ";
         i++;
     }
-    fout.close();
+    fw.close();
 }
 
 void interpolacao()
 {
    //Arquivos de entrada e de saida
-    fstream in[NumeroArquivos/2];
+    /*fstream in[NumeroArquivos/2];
     fstream out[NumeroArquivos/2];
 
 
@@ -89,7 +95,7 @@ void interpolacao()
         string enderecoIn= "in"+num.str();
         in[i].open(enderecoIn, fstream::in);
     }
-   
+
    //Abre os arquivos de saida como out
     while(int i=0 < NumeroArquivos/2)
     {
@@ -101,5 +107,33 @@ void interpolacao()
     while(int i=0 < NumeroArquivos/2)
     {
         //FAZER
+    }*/
+}
+
+//Ordenação dos buffers temporarios é feita com quicksort
+int quicksort(int *a, int inicio, int fim){
+    int index;
+    if(inicio >= fim)
+        return 0;
+    {
+        index = particiona(a, inicio, fim);
+        quicksort(a, inicio, index-1);
+        quicksort(a, index+1, fim);
     }
+}
+
+int particiona(int *a, int inicio, int fim){
+    int i,j,pindex,pivo;
+    pindex = inicio;
+    pivo = a[fim];
+    for(i = inicio; i < fim;i++)
+    {
+        if(a[i] <= pivo)
+        {
+            swap(a[pindex], a[i]);
+            pindex++;
+        }
+    }
+    swap(a[pindex], a[fim]);
+    return pindex;
 }
